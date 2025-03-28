4888720009cc3c2aa655603db09b8a19c750e4f6
PaolaVC08 <zS23004729@estudiantes.uv.mx>
2025-03-27 22:40:22 -0600

    creacion de models y  services

---
+++ src/app/admin-header/admin-header.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { HeaderService } from '../services/header-service/header.service';
+import { Header }from '../models/header/header.model';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-admin-header',
+  templateUrl: './admin-header.component.html',
+  styleUrl: './admin-header.component.css'
+})
+export class AdminHeaderComponent {
+   itemCount: number = 0;
+   btnTxt: string = "Agregar";
+   header: Header[] = [];
+   myHeader: Header = new Header();
+    constructor(public headerService: HeaderService)
+    {
+        console.log(this.headerService);
+        this.headerService.getHeader().snapshotChanges().pipe(
+          map(changes =>
+            changes.map( c =>
+             ({ id: c.payload.doc.id, ...c.payload.doc.data() })
+            )
+          )
+        ).subscribe(data => {
+          this.header = data;
+          console.log(this.header);
+        });
+    }
+
+  agregarHeader(){
+    console.log(this.headerService);
+    this.headerService.createHeader(this.myHeader).then(() => {
+       console.log('Created new item successfully!');
+    });
+  }
+
+  deleteHeader(id? :string){
+    this.headerService.deleteHeader(id).then(() => {
+       console.log('Delete item successfully!');
+    });
+    console.log(id);
+  }
+
+}
